f1b4f8886b613ab79b9e3b696ba89fad9e99c91c
Alejandro <ale2fernandez2003@gmail.com>
2024-01-15 09:00:40 +0100

    commit inicial

---
+++ ComprobadorDeMultiplos.java
@@ -0,0 +1,22 @@
+
+/**
+ * Clase que comprueba si un numero es multiplo de otro
+ * mediante diversos metodos.
+ * 
+ * @author Alejandro Fernandez Fernandez 
+ * @version 0.1
+ */
+public class ComprobadorDeMultiplos
+{
+    public boolean esMultiploDe2 (int a){
+        return true;
+    }
+
+    public boolean esMultiploDe5 (int b){
+        return true;
+    }
+
+    public boolean esMultiploDe (int a, int b){
+        return true;
+    }
+}
